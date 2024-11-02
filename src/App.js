e0420bbdf12651c8a48adc0aacb9508d962d2efa
sadananda das <sadanandadas174@gmail.com>
2024-11-02 12:32:56 +0530

    use react-router-dom and some changes in nav bar

--- src/App.js
+++ src/App.js
@@ -2,32 +2,50 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm.js";
 import About from "./components/About.js";
+//import { Route } from "react-router";
 
 import React, { useState } from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   const [mode, setMode] = useState("light");
   const touggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "black";
+      document.title = "TextUtils - enable dark mode";
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
+      document.title = "TextUtils - enable light mode";
     }
   };
   return (
     <>
-      <Navbar
-        title="Text Format"
-        aboutText="About Us"
-        mode={mode}
-        touggleMode={touggleMode}
-      />
-      <div className="container my-3">
-        <TextForm heading="Enter the text in billow : " mode={mode}/>
-      </div>
-      <About mode={mode}/>
+      <Router>
+        <Navbar
+          title="Text Format"
+          aboutText="About Us"
+          mode={mode}
+          touggleMode={touggleMode}
+        />
+        {/* if we are not use exact -->
+        ..
+        /user --> component1
+        /user/home--> --> component1
+        .. */}
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route
+              exact
+              path="/"
+              element={
+                <TextForm heading="Enter the text in billow : " mode={mode} />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
