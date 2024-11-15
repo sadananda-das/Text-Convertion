b8d531c8ab2fe4ddd13da4ec4ddf2d0baf78b693
Sadananda Das <sadanandadas174@gmail.com>
2024-11-15 10:23:54 +0530

    some chenges in navbat & textform

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -23,9 +23,8 @@ export default function TextForm(props) {
   };
 
   const handleCopy = () => {
-    let text = document.getElementById("myBox");
-    text.select();
-    navigator.clipboard.writeText(text.value);
+    navigator.clipboard.writeText(text);
+    document.getSelection().removeAllRanges();
   };
 
   const [text, setText] = useState("");
